ad555052a2e915e29edeecc4084bdb5e7c7f0f2f
Shashank <shashank.k.chaudhary@gmail.com>
2023-04-09 02:22:09 +0530

    getVendor and addVendor updated

--- controllers/addVendor.js
+++ controllers/addVendor.js
@@ -4,21 +4,23 @@ const vendor = require('../models/vendorData');
 const user = require('../models/userData');
 const { missingKeys, errorResponse } = require('../helpers/basicFunctions');
 const { addAddress } = require('../services/address');
+const { STATUS } = require('../helpers/constants');
+const { MongooseError } = require('mongoose')
 
 // ! add option for business email, business phone number
-// ! make emailVerified and phoneVerified to be mandatory to register as vendor
+// ! make emailVerified and phoneVerified to be mandatory to register as vendor > or add it later to activate the account
 const registerAsVendor = async (req, res, next) => {
     console.log("registerAsVendor");
-    // const { userId, email, phoneNumber } = req.body;
-    if (missingKeys(req.user, ['userId', 'email', 'emailVerified', 'phoneNumber', 'phoneVerified']) > 0) 
-        return errorResponse(STATUS.UNAUTHORIZED, "Unauthorized access", next);
+    // // ! remove this check
     const { userId, email, emailVerified, phoneNumber, phoneVerified } = req.user;
     // get user with uid and get the phone Number
     const requiredKeys = missingKeys(req?.body, ['displayName', 'photoURL']);
     if (requiredKeys > 0)
         return errorResponse(STATUS.BAD_REQUEST, `Missing keys: ${requiredKeys.join(', ')}`, next);
     const { displayName, photoURL, address, businessPhoneNumber } = req.body;
-
+    // ! check if the user is already a vendor
+    if (!phoneNumber && !businessPhoneNumber) 
+        return errorResponse(STATUS.BAD_REQUEST, "Missing phone number", next);
     try {
         res.controller = "registerAsVendor";
         const vendorInfo = new vendor({
@@ -39,6 +41,11 @@ const registerAsVendor = async (req, res, next) => {
         return next();
     }
     catch (error) {
+        // to handle duplicate key error
+        if (error instanceof MongooseError && error.code === 11000) {
+            const duplicateKey = Object.keys(error.keyPattern)[0];
+            return errorResponse(STATUS.CONFLICT, `${duplicateKey} already registered`, next);
+        }
         if (error.status) return next(error);
         errorResponse(STATUS.INTERNAL_SERVER_ERROR, error.message);
     }
